b8c8df87339d0e1541e8c9efaa42cd073517b13d
Dee <dgrowette@gmail.com>
2021-10-12 21:43:43 -0700

    redid game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,16 @@
-var playerName = window.prompt("What is your robot's name?");
-// this creates a function named "fight"
-function fight() {
-    console.log(playerName)
-    console.log("This logs a string, good for leaving yourself a message");
-    console.log(10+10);
-    console.log("Our robot's name is "+playerName);
-}
+var playerName=window.prompt("What is your robot's name?");
+var playerHealth=100;
+var playerAttack=10;
+
+//You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName="Roborto";
+var enemyHealth=50;
+var enemyAttack=12;
+
+var fight=function() {
+    window.alert("Welcome to Robot Gladiators")
+};
+
 fight();
